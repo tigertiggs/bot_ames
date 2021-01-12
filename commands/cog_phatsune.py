9aa3207637fe3694b5b70323e6c049abb8079c40
tigertiggs <38276989+tigertiggs@users.noreply.github.com>
2021-01-12 12:43:24 +0800

    allow ames to delete test ames embeds

--- commands/cog_phatsune.py
+++ commands/cog_phatsune.py
@@ -150,7 +150,7 @@ class hatsuneCog(commands.Cog):
         if self.active_embeds.get(str(message.id), None) and not self.active_embeds.get(str(message.id), None) == user.id:
             await message.remove_reaction('⏹️', user)
             return
-        elif not message.author is guild.me:
+        elif not (message.author is guild.me or message.author.id == 757869389851787374):
             return
         elif not message.embeds:
             return
